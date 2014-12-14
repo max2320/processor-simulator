9621026297c69e96f3b1260c71aac3b84d141ce8
Andeson Rocha <anderson2320@gmail.com>
2014-12-14 07:08:34 -0200

    docs

---
+++ processor/example/auxRegister.js
@@ -0,0 +1,11 @@
+{
+    name: <ID DO COMPONENTE>,
+    showName: <NOME DO COMPONENTE>,
+    size: <QUANTIDADE DE SETORES>,
+    pointer: <REGISTRADOR PONTEIRO>,
+    css: {
+        top: <POSIÇÃO VERTICAL>,
+        left: <POSIÇÃO HORIZONTAL>,
+    },
+    bus: <CONEXÕES>
+}
